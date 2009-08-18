b8817355a0afc9c65df5dde360c5b8d99d3b1242
Scott Ritchie <scottritchie@ubuntu.com>
2009-08-17 19:33:41 -0700

    Initial Release.  First version only has a template for Wine.

---
+++ gnome-exe-thumbnailer.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+f=`mktemp`
+
+if wrestool "$1" -x -t14 > $f && [ -s $f ]; then
+	id=$(icotool -l $f | awk '{
+		ci=int(substr($2,index($2,"=")+1));
+		cw=int(substr($3,index($3,"=")+1));
+		cb=int(substr($5,index($5,"=")+1));
+
+		if (cw > w || (cw == w && cb > b)) {
+			b = cb;
+			w = cw;
+			i = ci;
+		}
+		}
+		END {
+			print i;
+		}')
+
+	icotool -x --index=$id $f -o $f
+
+	if [ $(file -b $f | cut -f3 -d' ') -gt 48 ]; then
+		convert -resize 48x48 $f $f
+	fi
+	
+	if [[ $3 == "--clean" ]]; then
+		cp $f $2
+	else
+		composite -gravity center $f /usr/share/pixmaps/gnome-exe-thumbnailer-template-wine.png $2
+	fi
+else
+	cp /usr/share/pixmaps/gnome-exe-thumbnailer-template-wine.png "$2"
+fi
+
+rm $f
