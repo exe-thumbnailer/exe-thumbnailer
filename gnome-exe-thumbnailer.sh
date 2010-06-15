37053d122afa1bf74dc88ba3b09ba604975f5fd8
Scott Ritchie <scottritchie@ubuntu.com>
2010-06-15 13:36:09 -0700

    Port PPA changes to maverick

--- gnome-exe-thumbnailer.sh
+++ gnome-exe-thumbnailer.sh
@@ -1,37 +1,55 @@
 #!/bin/bash
 
-f=`mktemp`
-deurledfile=`python -c 'import sys, urlparse, urllib; print urllib.unquote(urlparse.urlparse(sys.argv[1]).path)' $1`
+TEMPFILE=$(mktemp)
+TEMPTHUMB=$(mktemp)
+DEURLEDFILE=$(python -c 'import sys, urlparse, urllib; print urllib.unquote(urlparse.urlparse(sys.argv[1]).path)' $1)
 
-if wrestool "$deurledfile" -x -t14 > $f && [ -s $f ]; then
-	id=$(icotool -l $f | awk '{
-		ci=int(substr($2,index($2,"=")+1));
-		cw=int(substr($3,index($3,"=")+1));
-		cb=int(substr($5,index($5,"=")+1));
+if wrestool --extract --type=group_icon "$DEURLEDFILE" > $TEMPFILE && [ -s $TEMPFILE ]
+then
+	read INDEX OFFSET < <(
+		icotool --list $TEMPFILE | awk '{
+			ci=int(substr($2,index($2,"=") + 1));
+			cw=int(substr($3,index($3,"=") + 1));
+			cb=int(substr($5,index($5,"=") + 1));
 
-		if (cw > w || (cw == w && cb > b)) {
-			b = cb;
-			w = cw;
-			i = ci;
-		}
+			if ((cw > w && cw <= 32) || (cw == w && cb > b)) {
+				b = cb;
+				w = cw;
+				i = ci;
+			}
 		}
 		END {
-			print i;
-		}')
+			print i, 16 + (32 - w) / 2;
+		}'
+	)
 
-	icotool -x --index=$id $f -o $f
+	icotool --extract --index=$INDEX $TEMPFILE -o $TEMPFILE
+	composite -geometry +$OFFSET+$OFFSET $TEMPFILE /usr/share/pixmaps/gnome-exe-thumbnailer-template.png $TEMPTHUMB
 
-	if [ $(file -b $f | cut -f3 -d' ') -gt 48 ]; then
-		convert -resize 48x48 $f $f
-	fi
-	
-	if [[ $3 == "--clean" ]]; then
-		cp $f $2
-	else
-		composite -gravity center $f /usr/share/pixmaps/gnome-exe-thumbnailer-template-wine.png $2
-	fi
 else
-	cp /usr/share/pixmaps/gnome-exe-thumbnailer-template-wine.png "$2"
+	grep -i "\.exe$" <<< "$DEURLEDFILE" && EXT="-x"
+	cp /usr/share/pixmaps/gnome-exe-thumbnailer-generic${EXT}.png $TEMPTHUMB
 fi
 
-rm $f
+if wrestool --extract --raw --type=version "$DEURLEDFILE" > $TEMPFILE && [ -s $TEMPFILE ]
+then
+	VERSION=$(cat $TEMPFILE \
+		| tr '\0' '\t' \
+		| sed 's/\t\t/#/g' \
+		| tr -c -d '[:print:]' \
+		| sed -r 's/.*Version#*([^#]*).*/\1/; s/, /./g;' \
+		| xargs echo \
+		| sed -r 's/^([0-9]*\.[0-9]*\.[0-9]).*/\1/' \
+		| sed 's/.* //'
+	)
+
+	if [ "$VERSION" ]
+	then
+		convert -font Helvetica-Bold -pointsize 7 -background '#00001090' -fill white label:" $VERSION " miff:- | \
+		composite -gravity southeast -geometry +1+3 - $TEMPTHUMB $2
+	fi
+else
+	cp $TEMPTHUMB $2
+fi
+
+rm $TEMPFILE $TEMPTHUMB
